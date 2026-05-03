a4391661cd5a594d528aee83012ad7ea995d6f46
Igor Polvora <igorpolvora>
2026-05-03 02:02:23 -0300

    JPA repository, dependency injection, database seeding

---
+++ demo/src/main/java/com/know/demo/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package com.know.demo.repositories;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.know.demo.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    
+}
